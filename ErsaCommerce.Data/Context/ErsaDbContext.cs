c7b9d5b4137b0f7914e0944c9c7e2397c351055f
Nyxola <88090162+Nyxolaa@users.noreply.github.com>
2025-06-19 22:26:36 +0300

    first migration for create db and tables

--- ErsaCommerce.Data/Context/ErsaDbContext.cs
+++ ErsaCommerce.Data/Context/ErsaDbContext.cs
@@ -5,10 +5,12 @@ namespace ErsaCommerce.Data
 {
     public class ErsaDbContext : DbContext, IErsaDbContext
     {
+        public ErsaDbContext(DbContextOptions<ErsaDbContext> options) : base(options) { }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> Orders { get ; set ; }
-        public DbSet<OrderItem> OrderItems { get ; set ; }
-        public DbSet<Product> Products { get ; set ; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }

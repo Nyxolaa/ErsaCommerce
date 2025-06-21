67d13b3d0912d9bef0c69d7b8aa81ce977af46fd
Nyxola <88090162+Nyxolaa@users.noreply.github.com>
2025-06-21 03:49:44 +0300

    swagger config

--- ErsaCommerce.Api/Program.cs
+++ ErsaCommerce.Api/Program.cs
@@ -1,3 +1,4 @@
+using ErsaCommerce.Api;
 using ErsaCommerce.Application;
 using ErsaCommerce.Data;
 using ErsaCommerce.Infrastructure;
@@ -5,7 +6,6 @@ using ErsaCommerce.Infrastructure.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +20,7 @@ DependencyInjection.AddApplication(builder.Services);
 
 builder.Services.AddDbContext<ErsaDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSwaggerConfig();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -40,36 +41,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "HeroF2.Api", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
-
-                    },
-                    new List<string>()
-                }
-            });
-    c.CustomSchemaIds(type => type.FullName);
-});
 
 var app = builder.Build();
 
